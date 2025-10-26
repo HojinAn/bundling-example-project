e164248523092a9b9da650aeaf88ecdc50e8be5c
HojinAn <devian3143@gmail.com>
2025-10-26 15:26:36 +0900

    date-fns dns 로드 삭제하고 의존성으로 추가 후에 가져와서 빌드하기 기존 cdn에서는 date-fnd 전체 불러온데 반해 format 함수만 tree shaking

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // emoji.js에서 필요한 것만 가져오기
 import { emojis } from './emoji.js';
+import { format } from 'date-fns';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const today = new Date();
-  // date-fns는 아직  CDN을 통해 불러오고 있어서 여기서는 직접 import하지 않음.
-  const formattedDate = dateFns.format(today, 'MMMM d, yyyy');
+  // date-fns 모듈에서 가져온 format 함수를 호출
+  const formattedDate = format(today, 'MMMM d, yyyy');
   document.getElementById('dateDisplay').textContent = formattedDate;
-  
+
   showRandomEmoji();
 });
 
 function showRandomEmoji() {
   const randomIndex = Math.floor(Math.random() * emojis.length);
   const selectedEmoji = emojis[randomIndex];
-  
+
   document.getElementById('emojiDisplay').textContent = selectedEmoji.icon;
   document.getElementById('emojiName').textContent = selectedEmoji.name;
 }
