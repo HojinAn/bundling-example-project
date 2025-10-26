1d5b2a60e47c25f04fd893d59fefb1710f212f86
HojinAn <devian3143@gmail.com>
2025-10-26 15:00:31 +0900

    emoji.js 스크립트를 따로 사용하지 않고 하나의 번들 파일에 들어가도록 하기

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// emoji.js에서 필요한 것만 가져오기
+import { emojis } from './emoji.js';
+
 document.addEventListener('DOMContentLoaded', function() {
   const today = new Date();
+  // date-fns는 아직  CDN을 통해 불러오고 있어서 여기서는 직접 import하지 않음.
   const formattedDate = dateFns.format(today, 'MMMM d, yyyy');
   document.getElementById('dateDisplay').textContent = formattedDate;
   
